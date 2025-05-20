8331d7dbda211c978302274c011853ea76dca48b
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-20 15:26:29 +0200

    DisplayServer: Started

---
+++ src/types/displayserver.rs
@@ -0,0 +1,13 @@
+
+
+pub struct WlCopy {
+    debian: &'static[&'static str],
+    fedora: &'static[&'static str],
+    arch: &'static[&'static str],
+}
+
+pub struct XorgWM {
+    debian: &'static[&'static str],
+    fedora: &'static[&'static str],
+    arch: &'static[&'static str],
+}
