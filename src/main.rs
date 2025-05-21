dfd8671416a5c6c427643b4143ab96dd559ee7f7
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-21 11:54:24 +0200

    removed displayserver.rs

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,7 @@ mod utils;
 // Used modules and types
 
 fn main() {
-    let transfer = types::transfer::Transfer::get_transfer();
+    //let transfer = types::transfer::Transfer::get_transfer();
+    let char = utils::menu::print_menu("Choose a compositor", 
+        &["[H]yprland","[R]iver","[S]way",]);
 }
