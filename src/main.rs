c53229bafc7a1c01ff1f1b60be3eeef923a7b2f8
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-23 14:13:52 +0200

    System: install()

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,18 @@ mod utils;
 
 
 // Used modules and types
+use types::system;
 
 fn main() {
     //let transfer = types::transfer::Transfer::get_transfer();
-    let char = utils::menu::print_menu("Choose a compositor", 
-        &["[H]yprland","[R]iver","[S]way",]);
+    //let char = utils::menu::print_menu("Choose a compositor", 
+        //&["[H]yprland","[R]iver","[S]way",]);
+
+    let sys = {
+        match system::System::get() {
+            Ok(a) => a,
+            Err(_) => return,
+        }
+    };
+    sys.install();
 }
--- src/types/system.rs
+++ src/types/system.rs
@@ -1,9 +1,8 @@
+use crate::FgColor;
+
 // Used modules
-use super::{display::DspServer, distro::Distro, transfer::Transfer};
-use std::io;
-
-
-
+use super::{display::DspServer, display::WlComp, display::XorgWM, distro::Distro, transfer::Transfer, };
+use std::{io, process::Command, };
 
 pub struct System<'a> {
     distro: Distro<'a>,
@@ -44,5 +43,131 @@ impl<'a> System <'a> {
             display: dsp,
             transfer: tr,
         });
+    }
+
+    pub fn install(&self) {
+        // This also stores the install cmd
+        let mut pacs: Vec<&str> = Vec::new();
+
+        // Get reference to suffix
+        let mut suffix: Option<&'a [&'a str]> = None;
+
+        // Add install cmd and base packs
+        match &self.distro {
+            Distro::Debian(_distro) => {
+                // Add install cmd
+                for var in _distro.install {
+                    pacs.push(var);
+                }
+                // Add packs
+                for var in _distro.basepkg {
+                    pacs.push(var);
+                }
+                suffix = _distro.suffix;
+            }
+            Distro::Fedora(_distro) => {
+                // Add install cmd
+                for var in _distro.install {
+                    pacs.push(var);
+                }
+                // Add packs
+                for var in _distro.basepkg {
+                    pacs.push(var);
+                }
+                suffix = _distro.suffix;
+            }
+            Distro::Arch(_distro) => {
+                // Add install cmd
+                for var in _distro.install {
+                    pacs.push(var);
+                }
+                // Add packs
+                for var in _distro.basepkg {
+                    pacs.push(var);
+                }
+                suffix = _distro.suffix;
+            }
+            Distro::Unknown => todo!(), // TODO: Figure out what to do here.
+        }
+
+
+        match &self.display {
+            DspServer::Xorg(xorg_wm, items) => {
+                // Append Xorg packages
+                for var in *items {
+                    pacs.push(var);
+                }
+                // Get WM packages
+                let wmpacs = {
+                    match xorg_wm {
+                        XorgWM::Awesome(items) => items,
+                        XorgWM::Bspwm(items) => items,
+                        XorgWM::I3(items) => items,
+                    }
+                };
+                // Append WM packages
+                for var in *wmpacs {
+                    pacs.push(var);
+                }
+            }
+            DspServer::Wayland(wl_comp, items) => {
+                // Append Wayland packages
+                for var in *items {
+                    pacs.push(var);
+                }
+                // Get Compositor packages
+                let wmpacs = {
+                    match wl_comp {
+                        WlComp::Hyprland(items) => items,
+                        WlComp::River(items) => items,
+                        WlComp::Sway(items) => items,
+                    }
+                };
+                // Append Compositor packages
+                for var in *wmpacs {
+                    pacs.push(var);
+                }
+            }
+            DspServer::Tty => todo!(), // TODO: Figure out what to do here.
+        }
+
+        // Append suffix
+        if let Some(val) = suffix {
+            for var in val {
+                pacs.push(var);
+            }
+        }
+
+        // Perform installaion
+        let installres = Command::new("sudo")
+            .args(pacs)
+            .spawn();
+
+        let mut install = {
+            match installres {
+                Ok(x) => x,
+                Err(e) => {
+                    println!(
+                        "{}Subproc creation failed: {e:?}{}", 
+                        FgColor!(Red),
+                        FgColor!(),
+                    );
+                    return ;
+                },
+            }
+        };
+
+
+        // Wait for subproc to finish
+        match install.wait() {
+            Ok(_) => {}
+            Err(e) => {
+                println!(
+                    "{}Command was not running {e:?}{}",
+                    FgColor!(Red),
+                    FgColor!(),
+                );
+            }
+        }
     }
 }
