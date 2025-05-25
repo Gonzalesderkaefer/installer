9328af85ddea54b095718d89d35e66c3e8491451
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-25 15:09:54 +0200

    Def: works like a config file

---
+++ src/def.rs
@@ -0,0 +1,5 @@
+/// This file defines all constants that are useful for main.rs
+
+/// The source of all files that will land in ~/.config or similar
+pub static CFGSRC: &'static str = "Jazzian/dotfiles";
+pub static CFGDEST: &'static str = ".config/";
