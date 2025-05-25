f83a0092e422dfa33c5167d569124a6c9389b346
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-05-25 17:56:24 +0200

    Def: added constants

--- src/def.rs
+++ src/def.rs
@@ -1,5 +1,102 @@
 /// This file defines all constants that are useful for main.rs
+/// If file paths are used make then relative to $HOME
 
 /// The source of all files that will land in ~/.config or similar
-pub static CFGSRC: &'static str = "Jazzian/dotfiles";
-pub static CFGDEST: &'static str = ".config/";
+pub static CFGSRC: &'static str = "Documents/code/Jazzian/dotfiles/config";
+pub static CFGDEST: &'static str = "Documents/code/Jazzian/.testdir";
+
+
+/// Custom scripts
+pub static BINSRC: &'static str = "Documents/code/Jazzian/dotfiles/bin";
+pub static BINDEST: &'static str = "Documents/code/Jazzian/.testbin";
+
+
+
+
+/// Predifined file contents
+pub static CUSTOMIZED_SH: &'static str = "killshells() {\n\
+        pkill -KILL -u $USER -t tty1\n\
+    }\n\
+    export BROWSER_PREFIX=\"firefox\"\n\
+    [ \"$(tty)\" = \"/dev/tty1\" ] && (startx; killshells)\n";
+
+pub static _DEBMDMENU: &'static str = "#!/usr/bin/sh\n\
+                  if [ $XDG_SESSION_TYPE = \"wayland\" ]; then\n\
+                      exec wofi_dmenu;\n\
+                  else\n\
+                      exec rofi_dmenu;\n\
+                  fi\n";
+
+pub static _DEBMDRUN: &'static str = "#!/usr/bin/sh\n\
+                 if [ $XDG_SESSION_TYPE = \"wayland\" ]; then\n\
+                     exec wofi_app;\n\
+                 else\n\
+                     exec rofi_app;\n\
+                 fi\n";
+
+pub static MDMENU_CONTENT: &'static str = "#!/usr/bin/sh\n\
+                       exec rofi_dmenu\n";
+
+pub static MDRUN_CONTENT: &'static str = "#!/usr/bin/sh\n\
+                      exec rofi_app\n";
+
+pub static MYTERM_CONTENT: &'static str = "#!/bin/sh\n\
+                       case $XDG_SESSION_TYPE in\n\
+                           \"wayland\")\n\
+                               exec alacritty -o font.size=12 $@\n\
+                               ;;\n\
+                           *)\n\
+                               exec alacritty -o font.size=12 $@\n\
+                               ;;\n\
+                       esac\n";
+
+pub static STARTX_CONTENT: &'static str = "x11startup &\nexec i3\n";
+
+pub static X11STARTUP: &'static str = "#!/usr/bin/bash\n";
+
+
+pub static GTK3_CONFIG: &'static str = "[Settings]\n\
+                    gtk-theme-name=Adwaita-dark\n\
+                    gtk-icon-theme-name=Papirus-Dark\n\
+                    gtk-font-name=JetBrains Mono Light 12\n\
+                    gtk-cursor-theme-size=0";
+
+
+
+/// Customized files
+pub static CUSTOMIZED: &'static [(&str, &str, u32)] =
+&[
+    // Customized files for window managers/compositors
+    (".config/i3/customzied/customzied", "", 0644),
+    (".config/bspwm/customzied/customzied", "", 0644),
+    (".config/awesome/customzied/customzied.lua", "", 0644),
+    (".config/sway/customzied/customzied", "", 0644),
+    (".config/hypr/customzied/customzied", "", 0644),
+    (".config/river/customzied/customzied", "", 0644),
+
+
+    // Customized shell files
+    (".customized.sh", CUSTOMIZED_SH, 0644),
+    (".customrc", "", 0644),
+
+
+    // X11 Startup
+    (".local/bin/x11startup", X11STARTUP, 0755),
+
+
+    // Xinitrc
+    (".xinitrc", STARTX_CONTENT, 0644),
+
+
+    // Myterm
+    (".local/bin/myterm", MYTERM_CONTENT, 0755),
+
+
+    // GTK3
+    (".config/gtk-3.0/settings.ini", GTK3_CONFIG, 0644),
+
+
+    // Menus
+    (".local/bin/mdmenu", MDMENU_CONTENT, 0755),
+    (".local/bin/mdrun", MDRUN_CONTENT, 0755),
+];
